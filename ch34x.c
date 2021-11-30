6b9062a4719a25bb91ac4dea7bb6675c6c11988c
juliagoda <juliagoda.pl@protonmail.com>
2021-11-30 13:20:15 +0100

    Changed statement for KERNEL_VERSION

--- ch34x.c
+++ ch34x.c
@@ -929,7 +929,7 @@ static int ch34x_write( struct usb_serial_port *port,
 	return count;
 }
 
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5,13,0)) && GCC_VERSION >= 100100
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5,14,0)) && GCC_VERSION >= 100100
 static unsigned int ch34x_write_room( struct tty_struct *tty )
 {
 	struct usb_serial_port *port = tty->driver_data;
@@ -959,7 +959,7 @@ static int ch34x_write_room( struct usb_serial_port *port )
 	return room;
 }
 
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5,13,0)) && GCC_VERSION >= 100100
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5,14,0)) && GCC_VERSION >= 100100
 static unsigned int ch34x_chars_in_buffer( struct tty_struct *tty )
 {
 	struct usb_serial_port *port = tty->driver_data;
