7dbe232ab84f7568e5b33d22d711ef0ac61d7897
Jagoda Górska <juliagoda.pl@protonmail.com>
2023-01-20 17:41:56 +0100

    Update ch34x.c
    Author - @beautifulzzzz

--- ch34x.c
+++ ch34x.c
@@ -399,7 +399,7 @@ static int ch34x_get_baud_rate( unsigned int baud_rate,
 
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,27))
 static void ch34x_set_termios( struct tty_struct *tty,
-		struct usb_serial_port *port, struct ktermios *old_termios )
+		struct usb_serial_port *port, const struct ktermios *old_termios )
 {
 #else
 static void ch34x_set_termios( struct usb_serial_port *port,
